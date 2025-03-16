cc35625f96942ee0945230313a28722af8914a55
skykim5538 <skykim5538@gmail.com>
2025-03-16 17:22:25 +0900

    업로드 테스트

---
+++ Hello.java
@@ -0,0 +1,25 @@
+package ch01;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//ctrl+shift+L 단축키 모음
+		//ctrl+D 한줄삭제
+		//ctrl+alt+shift+down 행단위 복사
+		//alt+shift+A 멀티 컬럼 편집
+		//alt + up/down 행단위 이동
+		//ctrl + i 자동 들여쓰기
+		// ctrl +/ 주석 토클
+		/* 여러줄 주석 
+		 *
+		 */
+		// ctrl +space
+		System.out.println("sss");
+		System.out.println("Hello world");
+
+			
+			
+	}
+
+}
